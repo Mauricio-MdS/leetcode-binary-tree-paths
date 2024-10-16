b090ad27f70a7d357970286a08d57d2fe75efd63
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-10-16 17:06:50 -0300

    Improve performance through StringBuilder

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,18 +4,22 @@ import java.util.List;
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> paths = new ArrayList<>();
-        binaryTreePathsHelper(root, "", paths);
+        binaryTreePathsHelper(root, new StringBuilder(), paths);
         return paths;
     }
 
-    private void binaryTreePathsHelper(TreeNode root, String path, List<String> paths) {
+    private void binaryTreePathsHelper(TreeNode root, StringBuilder path, List<String> paths) {
+        int pathLength = path.length();
+        path.append(root.val);
+
         if (root.left == null && root.right == null) {
-            paths.add(path + root.val);
-            return;
+            paths.add(path.toString());
+        } else {
+            path.append("->");
+            if (root.left != null) binaryTreePathsHelper(root.left, path, paths);
+            if (root.right != null) binaryTreePathsHelper(root.right, path, paths);
         }
 
-        path = path + root.val + "->";
-        if (root.left != null) binaryTreePathsHelper(root.left, path, paths);
-        if (root.right != null) binaryTreePathsHelper(root.right, path, paths);
+        path.setLength(pathLength);
     }
 }
